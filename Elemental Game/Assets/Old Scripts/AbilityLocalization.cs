b74a22bab5bb0c96b120c275fe962cd066c171a9
IluzietheSensei <68753453+IluzietheSensei@users.noreply.github.com>
2021-06-06 12:39:36 +0300

    PlayerMovement-Update1
    You can move forward and backwords but you can t jump <33

---
+++ Elemental Game/Assets/Old Scripts/AbilityLocalization.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AbilityLocalization : MonoBehaviour
+{
+    public bool CanRun = false;
+    public bool CanJump = false;
+
+    public void OnTriggerStay(Collider other)
+    {
+        if (other.CompareTag("Jump"))
+            CanJump = true;
+        if (other.CompareTag("Run"))
+            CanRun = true;
+    }
+
+    public void OnTriggerExit(Collider other)
+    {
+        CanRun = false;
+        CanJump = false;
+    }
+
+}
