b20e5c4f8a0bf6c3d8f95821d2700cb6e9bc26eb
Ryuuramaru <rosumateisc@gmail.com>
2021-06-09 17:00:21 +0300

    Time Warp UI Added
    Vignette effect when slowing down
    A timeRemaining bar
    bonus: SMAA and possible ambient occlusion + timewarp mechanics: cooldown and limited timewarptime

--- Elemental Game/Assets/Old Scripts/TimeControl.cs
+++ Elemental Game/Assets/Old Scripts/TimeControl.cs
@@ -1,10 +1,18 @@
 using Chronos;
 using UnityEngine;
+using UnityEngine.Rendering.PostProcessing;
 
 public class TimeControl : MonoBehaviour
 {
     public float slowdownFactor;
-  
+
+    private float slowmoDuration;
+    private float initialDuration;
+
+    public float maxDuration;
+    public float slowmoCooldown;
+
+
     void Start()
     {
         
@@ -18,7 +26,11 @@ public class TimeControl : MonoBehaviour
         if (Input.GetKeyDown(KeyCode.LeftShift))
         {
             clock.LerpTimeScale(slowdownFactor, 0.4f);
+            initialDuration = clock.unscaledTime;
         }
+
+        slowmoDuration = clock.unscaledTime - initialDuration;
+        if(slowmoDuration > maxDuration) clock.LerpTimeScale(1, 0.4f);
 
         if (Input.GetKeyUp(KeyCode.LeftShift))
         {
---
+++ Elemental Game/Assets/TimeWarp.cs
@@ -0,0 +1,134 @@
+using UnityEngine;
+using System.Collections;
+using UnityEngine.Rendering.PostProcessing;
+using Chronos;
+public class TimeWarp : MonoBehaviour
+{
+    PostProcessVolume m_Volume;
+    Vignette m_Vignette;
+
+    public float slowdownFactor;
+    public float maxDuration;
+    public float cooldown;
+
+    public float availableTime;
+    private float timePassed;
+    private float initialTime;
+
+    private bool canWarp;
+    private bool countTime = false;
+    private bool regenerateTime = true;
+
+
+    void Start()
+    {
+        // Create an instance of a vignette
+        m_Vignette = ScriptableObject.CreateInstance<Vignette>();
+        m_Vignette.enabled.Override(true);
+
+        m_Vignette.intensity.Override(0f);
+
+        m_Vignette.smoothness.Override(1f);
+
+        m_Vignette.roundness.Override(0.9f);
+
+        m_Vignette.color.Override(Color.blue);
+
+        // Use the QuickVolume method to create a volume with a priority of 100, and assign the vignette to this volume
+        m_Volume = PostProcessManager.instance.QuickVolume(gameObject.layer, 100f, m_Vignette);
+
+        availableTime = maxDuration;
+        
+    }
+
+    void Update()
+    {
+        Clock clock = Timekeeper.instance.Clock("Player");
+
+        if (Input.GetKeyDown(KeyCode.LeftShift) && timePassed >= cooldown)
+        {
+            //StartCoroutine(TimePassed());
+            DoSlowTime();
+            countTime = true;
+            regenerateTime = false;
+
+            initialTime = clock.time;
+            timePassed = 0;
+        }
+
+        if(Input.GetKeyUp(KeyCode.LeftShift) && countTime)
+        {
+            DoNormalTime();
+            countTime = false;
+            regenerateTime = true;
+        }
+
+        if (countTime)
+        {
+            availableTime -= clock.deltaTime;
+            if (availableTime <= 0f)
+            {
+                DoNormalTime();
+                countTime = false;
+                regenerateTime = true;
+            }
+        }
+        if (regenerateTime)
+        {
+            if (availableTime <= maxDuration) availableTime += clock.deltaTime;
+            else availableTime = maxDuration;
+
+            timePassed += clock.deltaTime;
+        }
+    }
+
+    /*IEnumerator TimePassed()
+    {
+        Clock clock = Timekeeper.instance.Clock("Player");
+        float initialTime = clock.time;
+        float timePassed = 0f;
+
+        while(timePassed <= maxDuration)
+        {
+            Clock Pclock = Timekeeper.instance.Clock("Player");
+            timePassed = Pclock.time - initialTime;
+            if (Input.GetKeyUp(KeyCode.LeftShift)) yield return timePassed;
+        }
+
+        DoNormalTime();
+        yield return maxDuration;  
+    }*/
+
+    void DoSlowTime()
+    {
+        Clock Wclock = Timekeeper.instance.Clock("World");
+
+        Wclock.LerpTimeScale(slowdownFactor, 0.4f);
+
+        //StopCoroutine(VignetteLerp(0f, 0f));
+        StartCoroutine(VignetteLerp(0f, 0.4f));
+    }
+
+    void DoNormalTime()
+    {
+        Clock clock = Timekeeper.instance.Clock("World");
+
+        clock.LerpTimeScale(1, 0.4f);
+
+        //StopCoroutine(VignetteLerp(0f, 0f));
+        StartCoroutine(VignetteLerp(0.4f, 0f));
+    }
+
+    IEnumerator VignetteLerp(float startValue, float endValue)
+    {
+        float timeElapsed = 0;
+
+        while (timeElapsed < 0.4f)
+        {
+            m_Vignette.intensity.value = Mathf.Lerp(startValue, endValue, timeElapsed / 0.4f);
+            timeElapsed += Time.deltaTime;
+
+            yield return null;
+        }
+    }
+}
